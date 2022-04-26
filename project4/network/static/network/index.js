db90508c102b0ba2a7dd3411dc331c43f9cba25a
forrest-ghost <201992460@mail.dlut.edu.cn>
2022-04-26 11:00:47 +0800

    first edit

---
+++ project4/network/static/network/index.js
@@ -0,0 +1,93 @@
+function postedit(id){
+
+    fetch(`/apipost/${id}`)
+.then(response => response.json())
+.then(blogpost => {
+    cardbody = document.querySelector(`#card-body-${id}`);
+    cardbody.innerHTML = `<form id="editform-${blogpost.id}"><div class="form-group"><label for="textarea">Edit your publication:</label>
+    <textarea class="form-control" id="textarea-${blogpost.id}" rows="5">${blogpost.content}</textarea><br><button class="btn btn-primary" type="submit">Save</button></form>`;
+
+    const form = document.querySelector(`#editform-${blogpost.id}`);
+    form.onsubmit = () => {
+        const textarea = document.querySelector(`#textarea-${blogpost.id}`).value;
+        fetch(`/apipost/${blogpost.id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                content: textarea,
+                username: blogpost.username
+            })
+          })
+        postshow(`${blogpost.id}`);
+        return false;
+    }
+});
+}
+
+
+function postshow(id){
+    cardbody = document.querySelector(`#card-body-${id}`);
+    fetch(`/apipost/${id}`)
+.then(response => response.json())
+.then(blogpost => {
+    cardbody.innerHTML = `<p>${blogpost.content}</p>`;
+});
+}
+
+
+function postlikee(id,likedby){
+    console.log(`id:${id}\nliked by:${likedby}`)
+    fetch(`/apipost/${id}`)
+.then(response => response.json())
+.then(blogpost => {
+    console.log(blogpost);
+    const likescount = blogpost.likes
+
+    fetch(`/likesapi/${id}`, {
+        method: 'POST',
+        body: JSON.stringify({
+            "id":id,
+            "likedby":likedby[0],
+            "likes":likescount+1
+        })
+      })
+      .then(response => response.json())
+      .then(result => {
+            console.log(result);
+            likesdiv = document.querySelector(`#likes-${id}`)
+            likesdiv.innerHTML = `<p><img class="unlikebtn" id="unlikebtn-${id}" src="https://img.icons8.com/color/48/000000/pig.png"/> <b>${likescount+1}</b></p>`
+            unlikebtn = document.querySelector(`#unlikebtn-${id}`)
+            unlikebtn.onclick = ()=> {
+                postunlikee(id,likedby)
+                                       }
+      });
+});
+    }
+
+
+function postunlikee(id,unlikedby){
+    console.log(`id: ${id}\nunlikedby: ${unlikedby}`)
+    fetch(`/apipost/${id}`)
+    .then(response => response.json())
+    .then(bpost => {
+                console.log(bpost.likes);
+                const likescount = bpost.likes
+                fetch(`/likesapi/${id}`, {
+                    method: 'DELETE',
+                    body: JSON.stringify({
+                        "id": id,
+                        "unlikedby": unlikedby[0],
+                        "likes": likescount-1
+                    })
+                  })
+                  .then(response => response.json())
+      .then(result => {
+          console.log(result);
+          likesdiv = document.querySelector(`#likes-${id}`)
+          likesdiv.innerHTML = `<p><img class="likebtn" id="likebtn-${id}" src="https://img.icons8.com/ios-filled/50/000000/pig.png"/> <b>${likescount-1}</b></p>`
+          likebtn = document.querySelector(`#likebtn-${id}`)
+          likebtn.onclick = ()=> {
+            postlikee(id,unlikedby)
+                                    }
+      });
+                    });
+}
---
+++ project4/network/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("search", views.search, name="search"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("newpost",views.newpost,name="newpost"),
+    path("sendpost",views.sendpost,name="sendpost"),
+    path("apipost/<int:postid>",views.apipost,name="apipost"),
+    path("profile/<str:uname>",views.profile,name="profile"),
+    path("followapi/<str:name>",views.followapi,name="followapi"),
+    path("follow/<str:name>",views.follow,name="follow"),
+    path("unfollow/<str:name>",views.unfollow,name="unfollow"),
+    path("followpost",views.followpost,name="followpost"),
+    path("likesapi/<int:postid>",views.likesapi,name="likesapi")
+]
